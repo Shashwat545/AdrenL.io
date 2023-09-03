164e2bfbe42fed4e829a400f964fc7046db89f26
Shashwat Singh <shashwatsingh545@gmail.com>
2023-09-03 16:16:45 +0530

    Addition of basic modal structure

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
-import UserMenu from "./UserMenu"
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
     return (
