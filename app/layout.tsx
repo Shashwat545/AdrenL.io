2c3b20d3f6bf66c585f1151da8df79674a822432
Shashwat Singh <shashwatsingh545@gmail.com>
2023-09-26 06:10:39 +0530

    Hot Reload error rectified

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import {Nunito} from "next/font/google";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
