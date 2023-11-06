188e9a833806c5a17f2556f5227c4ab2f9699bff
Shashwat Singh <shashwatsingh545@gmail.com>
2023-11-06 17:01:13 +0530

    Minor change

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -18,7 +18,7 @@ const EmptyState: React.FC<EmptyStateProps> = ({ title="No exact matches", subti
             <Heading center title={title} subtitle={subtitle}/>
             <div className="w-48 mt-4">
                 {showReset && (
-                    <Button outline label="Remove all filter" onClick={() => router.push('/')}/>
+                    <Button outline label="Remove all filters" onClick={() => router.push('/')}/>
                 )}
             </div>
         </div>
