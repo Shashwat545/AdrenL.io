0b481504bc6c3e8db177c813000e037ad1d3f22f
Abhay Patel <84718652+AbhayPatel1@users.noreply.github.com>
2023-11-13 17:21:25 +0530

    added instant-switch button

---
+++ app/hosting/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HostingCard from '../components/hosting/card'
+
+interface SellerPageProps{
+    heading:string;
+}
+
+export default function SellerPage() {
+    return ( 
+    <div  className="flex flex-col justify-evenly items-center md:flex-row h-64 md:h-96 lg:h-128 bg-cover bg-center bg-no-repeat justify-content  bg-[url('https://images.unsplash.com/photo-1618083707368-b3823daa2726?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]"   >
+        <HostingCard heading="Instant Switch" />
+        <HostingCard heading="Cancellation Policy" />
+        <HostingCard heading="Instant Switch" />
+        <HostingCard heading="Instant Switch" />
+        <HostingCard heading="Instant Switch" />
+        <HostingCard heading="Instant Switch" />
+  </div>
+  
+  )
+}
