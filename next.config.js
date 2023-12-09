e06c0b337318addadeafe9a376a6affa19840d32
Shashwat Singh <shashwatsingh545@gmail.com>
2023-12-09 13:30:15 +0530

    Carousel and next/image updated for Next.js V14

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "lh3.googleusercontent.com",
-            "res.cloudinary.com"
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com'
+            }
         ]
     }
 }
