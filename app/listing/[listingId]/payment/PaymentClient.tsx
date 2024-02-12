85fc224cc006d47e86c3a1319c62e7a2cc095b03
Shashwat Singh <shashwatsingh545@gmail.com>
2024-02-13 03:03:46 +0530

    Discount coupon and number of people implemented

--- app/listing/[listingId]/payment/PaymentClient.tsx
+++ app/listing/[listingId]/payment/PaymentClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { User, Listing, FuturePricing } from "@prisma/client";
+import { User, Listing, FuturePricing, DiscountCoupons } from "@prisma/client";
 import Container from "@/app/components/Container";
 import PaymentConfirmation from "@/app/components/payment/PaymentConfirmation";
 
@@ -16,6 +16,7 @@ interface PaymentClientProps {
     currentUser: User | null;
     listing: Listing & {user: User};
     futurePrices: FuturePricing[];
+    allDiscountCoupons: DiscountCoupons[];
 }
 
 interface DateAndPriceObject {
@@ -23,7 +24,7 @@ interface DateAndPriceObject {
     price: number;
 }
 
-const PaymentClient: React.FC<PaymentClientProps> = ({ currentUser, listing, futurePrices}) => {
+const PaymentClient: React.FC<PaymentClientProps> = ({ currentUser, listing, futurePrices, allDiscountCoupons }) => {
     const loginModal = useLoginModal();
     const router = useRouter();
     const params = useSearchParams();
@@ -53,6 +54,32 @@ const PaymentClient: React.FC<PaymentClientProps> = ({ currentUser, listing, fut
     const [isLoading, setIsLoading] = useState(false);
     const [totalPrice, setTotalPrice] = useState(price);
     const [totalPeople, setTotalPeople] = useState(1);
+    const [coupon, setCoupon] = useState('');
+    const [couponApplied, setCouponApplied] = useState(false);
+    const [percentageOff, setPercentageOff] = useState(0);
+
+    const handleCouponSubmit = async () => {
+        const couponExists = allDiscountCoupons.some(c => c.coupon === coupon);
+        if (!couponExists) {
+            toast.error("Coupon code is invalid or does not exist.");
+            return;
+        }
+        try {
+            const response = await axios.post('/api/coupon', { coupon, userId: currentUser?.id });
+            if (response.status === 200) {
+                toast.success(`Coupon Applied: ${response.data.percentageOff} % off`);
+                setCouponApplied(true);
+                setPercentageOff(response.data.percentageOff);
+            } else if (response.status === 218) {
+                toast.error('This coupon has reached its maximum usage limit.');
+            } else {
+                
+                toast.error(`Failed to Apply Coupon`);
+            }
+        } catch (error: any) {
+            toast.error(`Error applying coupon ${error}`);
+        }
+    };
 
     const onCreateReservation = useCallback(() => {
         if(!currentUser) {
@@ -61,6 +88,8 @@ const PaymentClient: React.FC<PaymentClientProps> = ({ currentUser, listing, fut
         setIsLoading(true);
         axios.post(`/api/reservations`, {
             listingId: listing.id,
+            discountApplied: couponApplied,
+            discountCoupon: couponApplied ? coupon : "NA",
             startDate: new Date(startDate ?? ''),
             endDate: new Date(endDate ?? ''),
             totalPrice: totalPrice,
@@ -82,17 +111,19 @@ const PaymentClient: React.FC<PaymentClientProps> = ({ currentUser, listing, fut
         .finally(() => {
             setIsLoading(false);
         });
-    }, [totalPrice, totalPeople, listing.cancellationPolicy, startDate, endDate, listing.id, router, currentUser, loginModal]);
+    }, [totalPrice, totalPeople, couponApplied, coupon, listing.cancellationPolicy, startDate, endDate, listing.id, router, currentUser, loginModal]);
 
     useEffect(() => {
-        setTotalPrice(totalPeople*price);
-    }, [totalPeople, price]);
+        setTotalPrice(totalPeople*price*(1 - (percentageOff/100)));
+    }, [totalPeople, price, percentageOff]);
 
     return (
         <Container>
             <div className="max-w-screen-lg mx-auto">
                 <PaymentConfirmation onSubmit={onCreateReservation} dateValue={new Date(startDate ?? '')} listing={listing} disabled={isLoading} price={price} totalPrice={totalPrice}
-                totalPeople={totalPeople} onChangePeople={(value: number) => setTotalPeople(value)}/>
+                totalPeople={totalPeople} onChangePeople={(value: number) => setTotalPeople(value)} couponApplied={couponApplied}
+                coupon={coupon} onChangeCoupon={(value: string) => setCoupon(value)} onCouponSubmit={handleCouponSubmit}
+                percentageOff={percentageOff}/>
             </div>
         </Container>
     );
